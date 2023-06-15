bb15070933dcc0fb377a1dd4b825b6d4663999de
Diego <diego.araujos@outlook.com.br>
2023-06-14 22:39:24 -0300

    script orientado a objetos

--- scraping.py
+++ scraping.py
@@ -1,113 +1,93 @@
+# arquivo scraping_oop.py
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
-import re
+import os
 import requests
 import base64
-import os
+import re
 
-options = Options()
-options.add_argument('-headless')
+class WebScraper():
 
-cnpj = '11728688000147'
-raw = f'{os.getcwd()}/github_/data/raw/'
-os.mkdir(f'{raw}{cnpj}')
+    def __init__(self, cnpj):
+        self.cnpj = cnpj
+        self.url_base = 'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM?cnpjFundo='
+        self.modo_oculto = Options()
+        self.modo_oculto.add_argument('-headless')
+        self.navegador = webdriver.Firefox(options=None) # ou self.modo_oculto
 
+    def cria_diretorio(self, dir):
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        else:
+            print('Diretório já existente')
 
-def acessa_pagina_web(cnpj):
-    navegador = webdriver.Firefox(options=options)
-    URL = 'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM?cnpjFundo='
-    navegador.get(''.join([URL, cnpj]))
-    time.sleep(2)
-    return navegador
+    def acessar_pagina_web(self):
+        self.navegador.get(f'{self.url_base}{self.cnpj}')
+        time.sleep(1)
+       
+    def pressionar_botao_exibir_filtros(self):
+        self.navegador.find_element(By.ID, 'showFiltros').click()
+        time.sleep(1)
+    
+    def pressionar_dropdown(self, id):
+        self.navegador.find_element(By.ID, id).click()
+        time.sleep(1)
 
+    def informar_valor_campo_texto(self, id, categoria):
+        self.navegador.find_element(By.ID, id).send_keys(categoria, Keys.ENTER)
+        time.sleep(1)
+    
+    def pressionar_botao_filtrar(self):
+        self.navegador.find_element(By.ID, 'filtrar').click()
+        time.sleep(1)
+    
+    def modificar_numero_registro_pagina(self):
+        seletor = Select(self.navegador.find_element(By.NAME, 'tblDocumentosEnviados_length'))
+        seletor.select_by_visible_text('100')
+        time.sleep(1)
+     
+    def coletar_links_validos(self):
+        seletor = self.navegador.find_elements(By.TAG_NAME, 'a')
+        links = [link.get_attribute('href') for link in seletor]
+        links_limpos = [link for link in links if link is not None] # remove tipos indefinidos
+        return [link for link in links_limpos if 'downloadDocumento' in link] # links de downloads
 
-def pressiona_botao_exibir_filtros(id):
-    exibir_filtros = navegador.find_element(By.ID, id)
-    exibir_filtros.click()
-    time.sleep(2)
+    def download_arquivo(self, dir, links):
+        for link in links:
+            doc = requests.get(link, allow_redirects=True, verify=True)
+            nome = re.findall('filename="(.+)"', doc.headers.get('content-disposition'))[0]
+            path = f'{dir}{nome}'
 
-
-def pressiona_dropdown(id):
-    dropdown = navegador.find_element(By.ID, id)
-    dropdown.click()
-    time.sleep(2)
-
-
-def informa_valor_campo_texto(id, categoria):
-    campo_texto = navegador.find_element(By.ID, id)
-    campo_texto.send_keys(categoria, Keys.ENTER)
-    time.sleep(2)
-
-
-def pressiona_botao_filtrar(id):
-    filtrar = navegador.find_element(By.ID, id)
-    filtrar.click()
-    time.sleep(2)
-
-
-def modifica_numero_registros_visiveis(num, nome):
-    mostrar_registros = navegador.find_element(By.NAME, nome)
-    registros = Select(mostrar_registros)
-    registros.select_by_visible_text(str(num))
-    time.sleep(2)
-
-
-def obtem_links(atributo):
-    todos_links = navegador.find_elements(By.TAG_NAME, 'a')
-    links = []
-    for link in todos_links:
-        links.append(link.get_attribute(atributo))
-    return links
-
-
-def remove_tipo_indefinido(lista):
-    links_definidos = [link for link in lista if link is not None]
-    return links_definidos
+            with open(path, 'wb') as arquivo:
+                arquivo.write(base64.b64decode(doc.content))
+            time.sleep(0.5)
+            print(nome)
 
 
-def remove_links_invalidos(lista):
-    nome_filtro = 'downloadDocumento'
-    links = [link for link in lista if nome_filtro in link]
-    return links
+fundo_imob = '11728688000147'
+caminho = f'{os.getcwd()}/dados/' #'/home/diego/Projetos/WebscrapingFundosNet/dados/'
 
+fii = WebScraper(fundo_imob)
+fii.cria_diretorio(f'{caminho}{fundo_imob}')
 
-def download_arquivo(caminho):
-    for link in links_validos:
-        doc = requests.get(link, allow_redirects=True, verify=True)
-        nome = re.findall('filename="(.+)"', doc.headers.get('content-disposition'))[0]
-        diretorio = ''.join([caminho, nome])
+fii.acessar_pagina_web()
+fii.pressionar_botao_exibir_filtros()
 
-        with open(diretorio, 'wb') as arquivo:
-            arquivo.write(base64.b64decode(doc.content))
-        time.sleep(0.5)
+fii.pressionar_dropdown('select2-chosen-2') # categoria
+fii.informar_valor_campo_texto('s2id_autogen2_search', 'Informes Periódicos')
 
+fii.pressionar_dropdown('select2-chosen-3') # tipo
+fii.informar_valor_campo_texto('s2id_autogen3_search', 'Informe Mensal Estruturado')
 
-navegador = acessa_pagina_web(cnpj)
+fii.pressionar_dropdown('select2-chosen-5') # situação
+fii.informar_valor_campo_texto('s2id_autogen5_search', 'Ativo')
 
-pressiona_botao_exibir_filtros('showFiltros')
+fii.pressionar_botao_filtrar() # aplica todos os filtros definidos
+fii.modificar_numero_registro_pagina()
 
-pressiona_dropdown('select2-chosen-2')  # Categoria
-informa_valor_campo_texto('s2id_autogen2_search', 'Informes Periódicos')
-
-pressiona_dropdown('select2-chosen-3')  # Tipo
-informa_valor_campo_texto('s2id_autogen3_search', 'Informe Mensal Estruturado')
-
-pressiona_dropdown('select2-chosen-5')  # Situação
-informa_valor_campo_texto('s2id_autogen5_search', 'Ativo')
-
-pressiona_botao_filtrar('filtrar')
-modifica_numero_registros_visiveis(100, 'tblDocumentosEnviados_length')
-
-links = obtem_links('href')
-
-navegador.quit()
-
-links = remove_tipo_indefinido(links)
-links_validos = remove_links_invalidos(links)
-
-download_arquivo(f'{raw}{cnpj}/')
-print('Download dos arquivos xml concluído')
+links = fii.coletar_links_validos()
+fii.download_arquivo(f'{caminho}{fundo_imob}/', links)
